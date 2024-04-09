60b5dc6f79c1677e0d759b02f0b1bdc13abb2163
thinkcode <thinkcodestudio@qq.com>
2024-04-09 17:01:52 +0800

    feat: ok, read data in wav file

--- src/wav_player.v
+++ src/wav_player.v
@@ -10,10 +10,10 @@ module wav_player #(
     output reg [31:0] file_size,
     output reg [1:0] channel_len,
     output reg [31:0] sample_rate,
+    output reg [31:0] byte_rate,
     output reg [15:0] sample_bits_per,
-    output [31:0] data_size,
+    output reg [31:0] data_size,
     output reg outen,
-    output reg out_clk,
     output reg [15:0] out_data,
     output error
 );
@@ -79,8 +79,71 @@ module wav_player #(
                             state <= long_data == "WAVE" ? FMT : ERROR;
                         end
                     end
-
-
+                    FMT:begin
+                        if(bit_cnt == 1'b1)begin
+                            state <= long_data == "fmt " ? BLOCK_LEN : ERROR;
+                        end
+                    end
+                    BLOCK_LEN:begin
+                        if(bit_cnt == 1'b1)begin
+                            state <= CODE_FMT;
+                        end
+                    end
+                    CODE_FMT:begin
+                        if(bit_cnt == 1'b0)begin
+                            state <= CHANNEL_LEN;
+                        end
+                    end
+                    CHANNEL_LEN:begin
+                        if(bit_cnt == 1'b1)begin
+                            channel_len <= short_data_h;
+                            state <= SAMPLE_RATE;
+                        end
+                    end
+                    SAMPLE_RATE:begin
+                        if(bit_cnt == 1'b1)begin
+                            sample_rate <= long_data;
+                            state <= DATA_RATE;
+                        end
+                    end
+                    DATA_RATE:begin
+                        if(bit_cnt == 1'b1)begin
+                            byte_rate <= long_data;
+                            state <= BLOCK_ALIGN;
+                        end
+                    end
+                    BLOCK_ALIGN:begin
+                        if(bit_cnt == 1'b0)begin
+                            state <= SAMPLE_BITS_PER;
+                        end
+                    end
+                    SAMPLE_BITS_PER:begin
+                        if(bit_cnt == 1'b1)begin
+                            sample_bits_per <= short_data_h;
+                            state <= OTHER;
+                        end
+                    end
+                    OTHER:begin
+                        if(in_data == "da")begin
+                            state <= DATA;
+                        end
+                    end
+                    DATA:begin
+                        if(in_data == "ta")begin
+                            bit_cnt <= 1'b0;
+                            state <= DATA_SIZE;
+                        end
+                    end
+                    DATA_SIZE:begin
+                        if(bit_cnt == 1'b1)begin
+                            data_size <= long_data;
+                            state <= DATA_BIT;
+                        end
+                    end
+                    DATA_BIT:begin
+                        outen <= 1'b1;
+                        out_data <= in_data;
+                    end
                 endcase
             end
         end
